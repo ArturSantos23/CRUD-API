1bf94975d0cf184acfab03b3c4ba0425c0a6dd59
Artur Santos <artur.correia.santos2002@gmail.com>
2022-05-25 19:02:56 +0100

    Final Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ import Edit from "./views/EditFilmes.view";
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand navbar-light bg-light">
         <a
           className="navbar-brand"
           href="https://www.estgv.ipv.pt/"
@@ -17,30 +17,17 @@ function App() {
         >
           www.estgv.ipv.pt
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          datatoggle="collapse"
-          data-target="#navbarSupportedContent"
-          ariacontrols="navbarSupportedContent"
-          aria-expanded="false"
-          arialabel="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" to="/">
-                {" "}
-                Lista de Filmes{" "}
-              </Link>
-            </li>
-          </ul>
-          <Link className="btn btn-success ms-auto" to="/form">
-            Adicionar Filme
-          </Link>
-        </div>
+        <ul className="navbar-nav me-auto">
+          <li className="nav-item active">
+            <Link className="nav-link" to="/">
+              {" "}
+              Lista de Filmes{" "}
+            </Link>
+          </li>
+        </ul>
+        <Link className="btn btn-success ms-lg-auto" to="/form">
+          Adicionar Filme
+        </Link>
       </nav>
       <div className="container py-4">
         <div className="row">
